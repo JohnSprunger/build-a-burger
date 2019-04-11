ff04d9adb6a77d5ce8c5a967e53527ecc22c6292
JohnSprunger <johnleesprunger@gmail.com>
2019-04-11 02:49:44 -0400

    Added build control component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     // Dynamically generating burger ingredients 
-    const newIngredients = Object.keys(props.ingredients)
+    let newIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />; 
             });   
-        }); 
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        
+    if(newIngredients.length === 0)
+        newIngredients = <p>Please start adding ingredients!</p> 
 
     return(
         <div className = {classes.Burger}> 
