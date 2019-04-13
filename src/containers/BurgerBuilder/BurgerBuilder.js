b260257ee1a1f98ca59b335b86214627f1628dcd
JohnSprunger <johnleesprunger@gmail.com>
2019-04-12 22:55:07 -0400

    Added Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger'; 
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
             lettuce: 1, 
-            bacon: 1, 
-            cheese: 2, 
-            meat: 2
+            bacon: 1,
+            meat: 1, 
+            cheese: 1 
         }
     }
 
+    addIngredientHandler = (type) => {
+
+    }
+
+    addIngredientHandler = (type) => {
+
+    }
 
     render(){
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} /> 
-                <div>Build Controls</div> 
+                <BuildControls />  
             </Aux> 
         ); 
     }
